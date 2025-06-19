407fb1b085efaa85495636c4af0856cefd67b4da
MasterDe <gthinkdesign520@gmail.com>
2025-06-19 17:48:12 +0800

    redirects

--- sidebar01/next.config.ts
+++ sidebar01/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config options here */ async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true, // 301 永久重定向（SEO友好）
+      },
+    ];
+  },
 };
 
 export default nextConfig;
